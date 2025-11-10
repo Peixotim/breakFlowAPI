3fa32054bf04df4dd5d0b6fd779fb0f7a99b278f
Peixotim <pedropeixotovz@gmail.com>
2025-11-10 12:07:31 -0300

    feat: Adiciona a instancia do banco de dados no `app.module.ts`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => {
+        const dbPortString = process.env.DB_PORT || '5432';
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(dbPortString, 10),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          autoLoadEntities: true,
+          synchronize: true, // Deixar true somente para desenvolvimento, em produção desativar
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
