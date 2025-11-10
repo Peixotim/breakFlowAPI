fbcbd1921a4a8f4890606e36483c716a7d5e8627
Peixotim <pedropeixotovz@gmail.com>
2025-11-10 16:42:07 -0300

    feat: Ajusta o codigo e adiciona o IsMail

--- src/enterprise/DTOs/enterprise-create.dto.ts
+++ src/enterprise/DTOs/enterprise-create.dto.ts
@@ -1,10 +1,46 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  IsEmail,
+  Length,
+} from 'class-validator';
 
 export class EnterpriseCreate {
   @IsString({ message: 'Error: The type passed must be string.¬' })
-  @IsNotEmpty({ message: 'Error: Company name cannot be empty.' })
+  @IsNotEmpty({ message: 'Error: The string cannot be empty.' })
   @MaxLength(255, {
     message: 'Error: The maximum number of characters is 255.',
   })
   companyName: string;
+
+  @IsString({ message: 'Error: The type passed must be string.¬' })
+  @IsNotEmpty({ message: 'Error: The string cannot be empty.' })
+  @MaxLength(255, {
+    message: 'Error: The maximum number of characters is 255.',
+  })
+  @IsOptional()
+  fantasyName: string;
+
+  @IsString({ message: 'Error: The type passed must be string.¬' })
+  @IsNotEmpty({ message: 'Error: The string cannot be empty.' })
+  @Length(14, 14, {
+    message: 'Error: The CNPJ must contain exactly 14 characters.',
+  })
+  cnpj: string;
+
+  @IsInt({ message: 'Error: The number must be of integer type.' })
+  numberOfEmployees: number;
+
+  @IsInt({ message: 'Error: The number must be of integer type.' })
+  numberOfSectors: number;
+
+  @IsEmail({}, { message: 'Error: The email address provided is invalid.' })
+  @MaxLength(255, {
+    message: 'Error: The maximum number of characters is 255.',
+  })
+  @IsOptional()
+  contactMail: string;
 }
