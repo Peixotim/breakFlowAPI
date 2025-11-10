99032cf6c1a740ff7b4918ec0b5935021fe8099b
Peixotim <pedropeixotovz@gmail.com>
2025-11-10 14:22:29 -0300

    feat: Adiciona o import de TypeOrmModule.forFeature()

--- src/enterprise/enterprise.module.ts
+++ src/enterprise/enterprise.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EnterpriseController } from './enterprise.controller';
 import { EnterpriseService } from './enterprise.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnterpriseEntity } from './entity/enterprise.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([EnterpriseEntity])],
   controllers: [EnterpriseController],
   providers: [EnterpriseService],
 })
