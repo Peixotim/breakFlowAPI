50efd076b861d7fca641c7b97f41ae6dec63006c
Peixotim <pedropeixotovz@gmail.com>
2025-11-11 16:39:36 -0300

    feat: Adiciona a rota de registro de empresas

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,3 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { EnterpriseEntity } from 'src/enterprise/entity/enterprise.entity';
+import { UsersEntity } from 'src/users/entity/users.entity';
+import { RegisterEnterpriseDto } from './DTOs/register.dto';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('register')
+  public async registerEnterprise(request: RegisterEnterpriseDto): Promise<{
+    enterprise: EnterpriseEntity;
+    owner: UsersEntity;
+  }> {
+    return await this.authService.registerEnterprise(request);
+  }
+}
