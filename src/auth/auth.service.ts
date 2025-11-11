d725c1b3edd017beb96db3e884466f5974583de2
Peixotim <pedropeixotovz@gmail.com>
2025-11-11 15:51:01 -0300

    feat: Começa as configurações do AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { EnterpriseService } from 'src/enterprise/enterprise.service';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly enterpriseService: EnterpriseService,
+    private readonly usersService: UsersService,
+  ) {}
+}
