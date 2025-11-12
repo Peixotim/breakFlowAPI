2d2fe42ea430b51b3b09bd2ca636bcf79de37b98
Peixotim <pedropeixotovz@gmail.com>
2025-11-12 20:06:27 -0300

    feat : Adiciona o controller de login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { EnterpriseEntity } from 'src/enterprise/entity/enterprise.entity';
 import { UsersEntity } from 'src/users/entity/users.entity';
 import { RegisterEnterpriseDto } from './DTOs/register.dto';
+import { LoginUser } from './DTOs/login.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
   @Post('register')
   public async registerEnterprise(
     @Body() request: RegisterEnterpriseDto,
@@ -14,5 +16,12 @@ export class AuthController {
     owner: UsersEntity;
   }> {
     return await this.authService.registerEnterprise(request);
+  }
+
+  @Post('login')
+  public async login(
+    @Body() request: LoginUser,
+  ): Promise<{ access_token: string }> {
+    return await this.authService.login(request);
   }
 }
