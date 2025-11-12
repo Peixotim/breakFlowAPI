bc724a600f1cdb4d0cad06c4f5d7b68a8c79c627
Peixotim <pedropeixotovz@gmail.com>
2025-11-12 20:06:40 -0300

    feat : Adiciona ao authmodule a configuracao do jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,25 @@ import { EnterpriseModule } from 'src/enterprise/enterprise.module';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [EnterpriseModule, UsersModule],
+  imports: [
+    EnterpriseModule,
+    UsersModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      global: true,
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET'),
+        signOptions: { expiresIn: '1w' },
+      }),
+
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
