f11c0169f62357fffdf49764fba6d924115165a7
Peixotim <pedropeixotovz@gmail.com>
2025-11-17 17:47:28 -0300

    feat: Começa as configurações do email

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
-
+import * as nodemailer from 'nodemailer';
 @Injectable()
-export class MailService {}
+export class MailService {
+  private transporter: nodemailer.Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: Number(process.env.EMAIL_PORT || 465),
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  }
+
+  public async sendMail(to: string, subject: string, content: string) {
+    const MailOptions = {
+      from: `"No Reply" <${process.env.EMAIL_FROM}>`,
+      to,
+      subject,
+      text: content,
+    };
+
+    await this.transporter.sendMail(MailOptions);
+  }
+}
