65d5d566781165e5ad3eb9d759b01b7bcc8de4fc
Peixotim <pedropeixotovz@gmail.com>
2025-11-17 17:04:57 -0300

    delete: Remove a importação desnecessaria

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
-import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [MailController],
   providers: [MailService],
-  imports: [UsersService],
 })
 export class MailModule {}
