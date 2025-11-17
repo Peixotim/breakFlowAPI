734a16c5d5a8392074346c957a583188f4475500
Peixotim <pedropeixotovz@gmail.com>
2025-11-17 17:58:59 -0300

    feat: Começa a estruturação do forgotPassword

--- src/password-reset/password.reset.service.ts
+++ src/password-reset/password.reset.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { RequestMailForgotPassword } from './DTOs/password-reset.dto';
+import { UsersService } from 'src/users/users.service';
 @Injectable()
-export class PasswordResetService {}
+export class PasswordResetService {
+  constructor(private readonly userService: UsersService) {}
+
+  public async forgotPassword(request: RequestMailForgotPassword) {
+    const user = await this.userService.findByMail(request.mail);
+    if (!user) {
+      throw new NotFoundException('Error, the email entered is invalid!');
+    }
+  }
+}
