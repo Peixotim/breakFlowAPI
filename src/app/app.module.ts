a655b9adbcbefaa65adf76a2583fba93a5ed90e8
Peixotim <pedropeixotovz@gmail.com>
2025-11-17 23:31:11 -0300

    feat: Faz a chamada das entidades no app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
 import { EnterpriseModule } from 'src/enterprise/enterprise.module';
+import { UsersEntity } from 'src/users/entity/users.entity';
+import { PasswordResetEntity } from 'src/password-reset/entity/password-reset.entity';
+import { EnterpriseEntity } from 'src/enterprise/entity/enterprise.entity';
 
 @Module({
   imports: [
@@ -15,15 +18,16 @@ import { EnterpriseModule } from 'src/enterprise/enterprise.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: () => {
-        const dbPortString = process.env.DB_PORT || '5432';
+        const dbPortString = process.env.DB_PORT || '5434';
         return {
           type: 'postgres',
-          host: process.env.DB_HOST,
+          host: 'localhost',
           port: parseInt(dbPortString, 10),
-          username: process.env.DB_USERNAME,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_DATABASE,
+          username: 'admin',
+          password: 'admin',
+          database: 'breakflow_db',
           autoLoadEntities: true,
+          entities: [UsersEntity, PasswordResetEntity, EnterpriseEntity],
           synchronize: true, // Deixar true somente para desenvolvimento, em produção desativar
         };
       },
