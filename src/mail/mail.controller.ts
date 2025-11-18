b7484579325bf51227e82fb54c78e565d984addc
Peixotim <pedropeixotovz@gmail.com>
2025-11-18 08:41:00 -0300

    feat: Adiciona a rota de envio de emails basico no controller do email

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { MailService } from './mail.service';
 
-@Controller()
-export class MailController {}
+@Controller('mail')
+export class MailController {
+  constructor(private readonly mailService: MailService) {}
+
+  //Envio de emails
+  @Post('send-mail')
+  public async sendMail(
+    to: string,
+    subject: string,
+    content: string,
+    htmlContent?: string,
+  ) {
+    return await this.mailService.sendMail(to, subject, content, htmlContent);
+  }
+}
