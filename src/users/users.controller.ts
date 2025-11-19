e9aa3a0923c918b2869a4eee4bd17e40b415166d
Peixotim <pedropeixotovz@gmail.com>
2025-11-19 20:33:29 -0300

    feat: Cria o controller de modificacao de usuarios

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  UnauthorizedException,
+  Body,
+  Controller,
+  Headers,
+  Patch,
+} from '@nestjs/common';
+import { ModifyUserDTO } from './DTOs/user-modify.dto';
+import { UsersService } from './users.service';
 
-@Controller()
-export class UsersController {}
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+  @Patch('modifyUser')
+  public async modifyUser(
+    @Headers('authorization') authHeader: string, //Recebe a sessionToken
+    @Body() request: ModifyUserDTO,
+  ) {
+    if (!authHeader) {
+      throw new UnauthorizedException('Token not found in request headers');
+    }
+    const accessToken = authHeader.split(' ')[1];
+    return await this.userService.modifyAccount(accessToken, request);
+  }
+}
