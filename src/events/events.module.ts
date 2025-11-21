5acc08e31b14ec8664a8708d1dcce8b2c86c9f9b
Peixotim <pedropeixotovz@gmail.com>
2025-11-21 16:26:24 -0300

    feat: Faz a importação do service de User

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,9 +3,10 @@ import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsEntity } from './entity/events.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EventsEntity])],
+  imports: [TypeOrmModule.forFeature([EventsEntity]), UsersModule],
   controllers: [EventsController],
   providers: [EventsService],
 })
