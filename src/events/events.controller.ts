d70681c04fb574ec5e8c7aabb0e1f5abe747cf46
Peixotim <pedropeixotovz@gmail.com>
2025-11-21 16:26:07 -0300

    feat: Cria o controller de events

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Headers,
+  Post,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import { EventsCreateDTO } from './DTOs/event-create.dto';
+import { EventsService } from './events.service';
+import { RolesGuard } from 'src/auth/guards/roles.guards';
 
-@Controller()
-export class EventsController {}
+@UseGuards(RolesGuard)
+@Controller('events')
+export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+  @Post('create-events')
+  public async createEvent(
+    @Headers('authorization') authHeader: string,
+    @Body() request: EventsCreateDTO,
+  ) {
+    if (!authHeader) {
+      throw new UnauthorizedException('Token not found in request headers');
+    }
+
+    const sessionToken = authHeader.split(' ')[1];
+
+    return this.eventsService.eventsRegister(sessionToken, request);
+  }
+}
