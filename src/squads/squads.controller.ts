f6335405d003bc9cb2710f9b4e59bfe47a26d063
Peixotim <pedropeixotovz@gmail.com>
2025-11-24 15:47:31 -0300

    feat: Cria os controllers , de criação de squads, adição de usuarios nos squads e uma rota get para pegar o squad via uuid

--- src/squads/squads.controller.ts
+++ src/squads/squads.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Headers,
+  Body,
+  UnauthorizedException,
+  Param,
+  Get,
+} from '@nestjs/common';
+import { OwnerGuard } from 'src/auth/guards/owner.guards';
+import { SquadRegister } from './DTOs/squads-register.dto';
+import { SquadService } from './squads.service';
+import { SquadsAdding } from './DTOs/squads-adding.dto';
 
-@Controller()
-export class SquadController {}
+@Controller('squad')
+export class SquadController {
+  constructor(private readonly squadService: SquadService) {}
+  @UseGuards(OwnerGuard)
+  @Post('register-squad')
+  public async registerSquad(
+    @Headers('authorization') authHeader: string,
+    @Body() request: SquadRegister,
+  ) {
+    if (!authHeader) {
+      throw new UnauthorizedException('Token not found in request headers');
+    }
+
+    const sessionToken = authHeader.split(' ')[1];
+
+    return this.squadService.squadRegister(request, sessionToken);
+  }
+
+  @Get(':uuid')
+  public async findByUuid(@Param('uuid') uuid: string) {
+    return this.squadService.findByUuid(uuid);
+  }
+
+  @Post('adding-to-squad/:uuid')
+  public async addingToSquad(
+    @Param('uuid') uuid: string,
+    @Headers('authorization') authHeader: string,
+    @Body() request: SquadsAdding,
+  ) {
+    if (!authHeader) {
+      throw new UnauthorizedException('Token not found in request headers');
+    }
+
+    const sessionToken = authHeader.split(' ')[1];
+
+    return await this.squadService.addingToSquad(request, sessionToken, uuid);
+  }
+}
