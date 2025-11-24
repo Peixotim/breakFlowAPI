50e49300fdc51152d28f7e6bfb39090e53647b3a
Peixotim <pedropeixotovz@gmail.com>
2025-11-24 16:34:29 -0300

    fix: Corrige o bug de ordem , onde a ordem estava invertida

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+
+  // 1. Configura Pipes (Validação) ANTES de tudo
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, //Remove chaves que não estão no DTO
-      forbidNonWhitelisted: true, //Levanta erro quando a chave não existe
-      transform: true, //Tenta transformar os tipos de dados de param e dtos
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
+
+  // 2. Configura Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Break Flow API')
+    .setDescription('Documentação da API do Break Flow')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
