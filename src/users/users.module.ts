b1a4e1a70d7aadedbeceecf575e2bb7740c90591
Peixotim <pedropeixotovz@gmail.com>
2025-11-24 15:35:15 -0300

    feat: Adiciona a importação de squad

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersEntity } from './entity/users.entity';
 import { EventsEntity } from 'src/events/entity/events.entity';
+import { SquadEntity } from 'src/squads/entity/squads.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersEntity, EventsEntity])],
+  imports: [TypeOrmModule.forFeature([UsersEntity, EventsEntity]), SquadEntity],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
